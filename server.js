3e689d6936ad71766882dd481a5ddb981fccf0b0
npressello <npressello@gmail.com>
2022-10-11 21:20:24 -0300

    Clase 7

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const Contenedor = require('./Contenedor.js');
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8080;
-let visitas = 0;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
@@ -11,12 +12,6 @@ const server = app.listen(PORT, () => {
 server.on('error', err => console.log(`Error on server ${err}`));
 
 const productos = new Contenedor('productos.txt');
-
-app.get('/visitas', (req, res) => {
-  console.log(req.ip);
-  visitas++;
-  res.send(`La cantidad de visitas es ${visitas}`);
-});
 
 app.get('/productos', async (req, res) => {
   console.log(req.ip);
@@ -31,5 +26,94 @@ app.get('/productoRandom', async (req, res) => {
   res.send(todosLosProductos[rand]);
 });
 
-app.all('*', (req, res) => res.send('Ruta no valida'));
+//app.all('*', (req, res) => res.send('Ruta no valida'));
 
+
+// EJERCICIOS BASICOS
+
+app.get('/api/sumar/:num1/:num2', (req, res) => {
+  let { num1, num2 } = req.params;
+  res.send({ suma: parseInt(num1) + parseInt(num2) });
+});
+
+
+app.get('/api/sumar', (req, res) => {
+  let { num1, num2 } = req.query;
+  res.send({ suma: parseInt(num1) + parseInt(num2) });
+});
+
+app.get('/api/operacion/:nums', (req, res) => {
+  let result = eval(req.params.nums);
+  res.send({ suma: result });
+});
+
+let frase = "Hola mundo como estan";
+
+app.post('/api/mensajes', (req, res) => {
+  console.log('POST request recibido');
+
+  console.log(req.body);
+  res.send("OK");
+});
+
+app.put('/api/mensajes-json/:id', (req, res) => {
+  console.log('PUT request recibido');
+
+  res.json({
+    result: 'ok',
+    id: req.params.id,
+    nuevo: req.body
+  });
+});
+
+// Ultimo ejercicio 7
+
+app.get('/api/frase', (req, res) => {
+  res.json({ frase: frase });
+})
+
+app.get('/api/letras/:num', (req, res) => {
+  res.json({ letra: (frase.charAt(parseInt(req.params.num)) || "El parámetro no es un número valido") });
+})
+
+app.get('/api/palabras/:pos', (req, res) => {
+  const arrFrase = frase.split(" ")
+  const pos = parseInt(req.params.pos) - 1 || -1;
+  if (pos < 0 || pos > arrFrase.length) {
+    res.json({ error: "palabra no encontrada" });
+    return;
+  }
+  res.json({ palabra: arrFrase[pos] });
+})
+
+app.post('/api/palabras', (req, res) => {
+  frase = frase + " " + req.body.palabra;
+  pos = frase.split(" ").length;
+  res.json({ agregada: req.body.palabra, pos: pos });
+})
+
+app.put('/api/palabras/:pos', (req, res) => {
+  let arrFrase = frase.split(" ");
+  const pos = parseInt(req.params.pos) - 1 || -1;
+  if (pos < 0 || pos > arrFrase.length) {
+    res.json({ error: "posicion incorrecta" });
+    return;
+  }
+  let anterior = arrFrase[pos];
+  arrFrase[pos] = req.body.palabra;
+  frase = arrFrase.join(" ");
+  res.json({ actualizada: arrFrase[pos], anterior: anterior });
+})
+
+app.delete('/api/palabras/:pos', (req, res) => {
+  let arrFrase = frase.split(" ");
+  const pos = parseInt(req.params.pos) - 1 || -1;
+  if (pos < 0 || pos > arrFrase.length) {
+    res.send({ error: "posicion incorrecta" });
+    return;
+  }
+
+  arrFrase = arrFrase.filter((e, ix) => ix != pos);
+  frase = arrFrase.join(" ");
+  res.json({ palabra: "Se elimino palabra" })
+})
